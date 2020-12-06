01f03af49731b0f7289673339ee28ab5febe3d74
tachukao <tachu.kao@gmail.com>
2020-12-06 14:41:18 +0000

    reorganized simple

--- examples/simple/simple.ml
+++ examples/simple/simple.ml
@@ -1,1 +1,5 @@
-let context = Wrapper.Context.create ()
+open Wrapper
+let context = Context.create ()
+
+let () =
+  Context.num_loaded_dialects context |> Printf.printf "number_of_loaded_dialects %i\n"
