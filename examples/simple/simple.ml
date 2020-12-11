58467ad189e801b91d7f65ca7719f4a86b3c078a
tachukao <tachu.kao@gmail.com>
2020-12-11 12:47:37 +0000

    minor bug

--- examples/simple/simple.ml
+++ examples/simple/simple.ml
@@ -1,8 +1,9 @@
 open Core
-
-let context = Context.create ()
+open IR
 
 let () =
+  let context = Context.create () in
   Context.num_loaded_dialects context |> Printf.printf "number_of_loaded_dialects %i\n";
   Context.num_registered_dialects context
-  |> Printf.printf "number_of_registered_dialects %i\n"
+  |> Printf.printf "number_of_registered_dialects %i\n";
+  Context.destroy context
